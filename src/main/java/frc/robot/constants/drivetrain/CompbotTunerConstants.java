612fbaac2e7da26b25d7ff7af142f9a2a9edbece
btho7793 <77469772+btho7793@users.noreply.github.com>
2025-01-18 17:42:51 -0500

    Decrease temporary pid gains

--- src/main/java/frc/robot/constants/drivetrain/CompbotTunerConstants.java
+++ src/main/java/frc/robot/constants/drivetrain/CompbotTunerConstants.java
@@ -24,7 +24,7 @@ public class CompbotTunerConstants {
     // The steer motor uses any SwerveModule.SteerRequestType control request with the
     // output type specified by SwerveModuleConstants.SteerMotorClosedLoopOutput
     private static final Slot0Configs steerGains = new Slot0Configs()
-        .withKP(100).withKI(0).withKD(0.5)
+        .withKP(20).withKI(0).withKD(0.5)
         .withKS(0.1).withKV(1.5).withKA(0)
         .withStaticFeedforwardSign(StaticFeedforwardSignValue.UseClosedLoopSign);
     // When using closed-loop control, the drive motor uses the control
