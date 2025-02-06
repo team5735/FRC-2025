bab0ca2473ccca1ba7a84b07695314373d83a341
Noah Goldstein <goldn2041@gmail.com>
2025-02-05 19:47:13 -0500

    feeder beginnings

---
+++ src/main/java/frc/robot/Constants/Constants.java
@@ -0,0 +1,5 @@
+package frc.robot.Constants;
+
+public class Constants {
+    public static final int FEEDER_NEO_ID = 1;
+}
---
+++ src/main/java/frc/robot/Constants/FeederConstants.java
@@ -0,0 +1,5 @@
+package frc.robot.Constants;
+
+public class FeederConstants {
+    public static final double FEEDER_NEO_VOLTS = 1.0;
+}
