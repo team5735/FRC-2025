64afe2e0023a900f371f8e2cbcb594fb7c71e6c4
btho7793 <77469772+btho7793@users.noreply.github.com>
2025-02-10 20:52:42 -0500

    Add devices and controllers to Elevator

--- src/main/java/frc/robot/constants/ElevatorConstants.java
+++ src/main/java/frc/robot/constants/ElevatorConstants.java
@@ -1,6 +1,19 @@
 package frc.robot.constants;
 
+import static edu.wpi.first.units.Units.MetersPerSecond;
+import static edu.wpi.first.units.Units.MetersPerSecondPerSecond;
+
+import edu.wpi.first.units.measure.LinearAcceleration;
+import edu.wpi.first.units.measure.LinearVelocity;
+
 public class ElevatorConstants {
-    public static final double ELEVATOR_KRAKEN_RIGHT_VOLTS = 1;
-    public static final double ELEVATOR_KRAKEN_LEFT_VOLTS = -1;
+    public static final int KP = 0;
+    public static final int KI = 0;
+    public static final int KD = 0;
+    public static final int KS = 0;
+    public static final int KG = 0;
+    public static final int KV = 0;
+    public static final int KA = 0;
+    public static final LinearVelocity MAX_VELOCITY = MetersPerSecond.of(0);
+    public static final LinearAcceleration MAX_ACCELERATION = MetersPerSecondPerSecond.of(0);
 }
--- src/main/java/frc/robot/subsystem/ElevatorSubsystem.java
+++ src/main/java/frc/robot/subsystem/ElevatorSubsystem.java
@@ -1,44 +1,43 @@
 package frc.robot.subsystem;
 
+import static edu.wpi.first.units.Units.MetersPerSecond;
+import static edu.wpi.first.units.Units.MetersPerSecondPerSecond;
+
+import com.ctre.phoenix6.controls.Follower;
+import com.ctre.phoenix6.hardware.TalonFX;
 import com.revrobotics.spark.SparkMax;
 
 import com.revrobotics.spark.SparkLowLevel.MotorType;
 import com.revrobotics.spark.config.SparkMaxConfig;
 
+import edu.wpi.first.math.controller.ElevatorFeedforward;
+import edu.wpi.first.math.controller.ProfiledPIDController;
+import edu.wpi.first.math.trajectory.TrapezoidProfile.Constraints;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.constants.Constants;
 import frc.robot.constants.ElevatorConstants;
 
 public class ElevatorSubsystem extends SubsystemBase {
-    private final SparkMax sparkMax_feed = new SparkMax(Constants.ELEVATOR_KRAKEN_RIGHT_ID, MotorType.kBrushless);
-    private final SparkMaxConfig pull_config = new SparkMaxConfig();
+    private final TalonFX krakenRight = new TalonFX(Constants.ELEVATOR_KRAKEN_RIGHT_ID);
+    private final TalonFX krakenLeft = new TalonFX(Constants.ELEVATOR_KRAKEN_LEFT_ID);
+    private ProfiledPIDController pid = new ProfiledPIDController(
+        ElevatorConstants.KP, ElevatorConstants.KI, ElevatorConstants.KD,
+        new Constraints(
+            ElevatorConstants.MAX_VELOCITY.in(MetersPerSecond), 
+            ElevatorConstants.MAX_ACCELERATION.in(MetersPerSecondPerSecond)
+        )
+    );
+    private ElevatorFeedforward ff = new ElevatorFeedforward(
+        ElevatorConstants.KS, ElevatorConstants.KG, ElevatorConstants.KV, ElevatorConstants.KA
+    );
 
-    public ElevatorSubsystem() {
-        sparkMax_feed.configure(pull_config.inverted(true), null, null);
+    public ElevatorSubsystem(){
+        krakenLeft.setControl(new Follower(Constants.ELEVATOR_KRAKEN_RIGHT_ID, true));
     }
 
-    public void feed() {
-        sparkMax_feed.setVoltage(ElevatorConstants.ELEVATOR_KRAKEN_RIGHT_VOLTS);
-    }
+    @Override
+    public void periodic(){
 
-    public void spit() {
-        sparkMax_feed.setVoltage(ElevatorConstants.ELEVATOR_KRAKEN_LEFT_VOLTS);
-    }
-
-    public void stop() {
-        sparkMax_feed.setVoltage(0);
-    }
-
-    public Command pullStopCommand() {
-        return startEnd(() -> feed(), () -> stop());
-    }
-
-    public Command pushStopCommand() {
-        return startEnd(() -> spit(), () -> stop());
-    }
-
-    public Command stopCommand() {
-        return runOnce(() -> stop());
     }
 }
