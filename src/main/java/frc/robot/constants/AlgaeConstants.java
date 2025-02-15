0a844ac290f2953531d4dc78b58dfe4f3d5a0b32
Jacoby Adwin <jacoby_adwin@student.waylandps.org>
2025-02-15 14:35:51 -0500

    Set Algae neutral mode to brake and rename constants to be more cohesive

--- src/main/java/frc/robot/constants/AlgaeConstants.java
+++ src/main/java/frc/robot/constants/AlgaeConstants.java
@@ -2,5 +2,5 @@ package frc.robot.constants;
 
 public class AlgaeConstants {
     public static final double GRAB_VOLTS = 1.0;
-    public static final double DROP_VOLTS = 1.0;
+    public static final double SPIT_VOLTS = 1.0;
 }
--- src/main/java/frc/robot/subsystems/AlgaeSubsystem.java
+++ src/main/java/frc/robot/subsystems/AlgaeSubsystem.java
@@ -1,6 +1,8 @@
 package frc.robot.subsystems;
 
+import com.ctre.phoenix6.configs.MotorOutputConfigs;
 import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.signals.NeutralModeValue;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
@@ -11,25 +13,26 @@ public class AlgaeSubsystem extends SubsystemBase {
     private final TalonFX falcon = new TalonFX(Constants.ALGAE_FALCON_ID);
 
     public AlgaeSubsystem() {
+        falcon.getConfigurator().apply(new MotorOutputConfigs().withNeutralMode(NeutralModeValue.Brake));
     }
 
-    public void feed() {
+    public void grab() {
         falcon.setVoltage(AlgaeConstants.GRAB_VOLTS);
     }
 
     public void spit() {
-        falcon.setVoltage(-AlgaeConstants.GRAB_VOLTS);
+        falcon.setVoltage(-AlgaeConstants.SPIT_VOLTS);
     }
 
     public void stop() {
         falcon.setVoltage(0);
     }
 
-    public Command pullStopCommand() {
-        return startEnd(() -> feed(), () -> stop());
+    public Command grabStopCommand() {
+        return startEnd(() -> grab(), () -> stop());
     }
 
-    public Command pushStopCommand() {
+    public Command spitStopCommand() {
         return startEnd(() -> spit(), () -> stop());
     }
 
