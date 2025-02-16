10b9aaef5cf9c6b874688038711e690d7482883d
btho7793 <77469772+btho7793@users.noreply.github.com>
2025-02-16 17:15:42 -0500

    Add semi-final voltages for CoralSubsystem branch and trough

--- src/main/java/frc/robot/constants/CoralConstants.java
+++ src/main/java/frc/robot/constants/CoralConstants.java
@@ -1,15 +1,15 @@
 package frc.robot.constants;
 
 public class CoralConstants {
-    //TODO FIND ACTUAL VOLTAGES
+    //TODO FIND ACTUAL VOLTAGES FOR INTAKE
     //TODO FIND ACTUAL DELAY
     public static final double INTAKE_TOP_VOLTS = 0.5;
     public static final double INTAKE_BOTTOM_VOLTS = 0.5;
 
-    public static final double TROUGH_TOP_VOLTS = 2;
-    public static final double TROUGH_BOTTOM_VOLTS = 1.5;
+    public static final double TROUGH_TOP_VOLTS = 1.5;
+    public static final double TROUGH_BOTTOM_VOLTS = 0.25;
 
-    public static final double BRANCH_TOP_VOLTS = 1.0;
+    public static final double BRANCH_TOP_VOLTS = 1.25;
     public static final double BRANCH_BOTTOM_VOLTS = 1.0;
     
     public static final double FEED_DELAY_SECONDS = 0;
