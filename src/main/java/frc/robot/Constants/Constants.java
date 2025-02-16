cd2118d794266e08268b2aa5c9903d481a4897cf
btho7793 <77469772+btho7793@users.noreply.github.com>
2025-02-16 13:53:03 -0500

    Fix redundancy missed in merge

--- src/main/java/frc/robot/Constants/Constants.java
+++ src/main/java/frc/robot/Constants/Constants.java
@@ -1,7 +1,6 @@
 package frc.robot.constants;
 
 public class Constants {
-    public static final int FEEDER_NEO_ID = 1;
     public static final int FEEDER_BEAM_ID = 12;
     public static final int FEEDER_FALCON_ID = 1;
 }
