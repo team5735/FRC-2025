154f4c09b42988dcf9d24146a8e6cc01af3d093f
btho7793 <77469772+btho7793@users.noreply.github.com>
2025-02-17 17:57:34 -0500

    Add rough command logic to ClimberSubsystem

--- src/main/java/frc/robot/constants/ClimberConstants.java
+++ src/main/java/frc/robot/constants/ClimberConstants.java
@@ -1,6 +1,13 @@
 package frc.robot.constants;
 
+import static edu.wpi.first.units.Units.Rotations;
+
+import edu.wpi.first.units.measure.Angle;
+
 public class ClimberConstants {
     public static final double OUT_VOLTS = 1.0;
     public static final double CLIMB_VOLTS = 1.0;
+
+    public static final Angle INNER_LIMIT = Rotations.of(0);
+    public static final Angle OUTER_LIMIT = Rotations.of(1);
 }
--- src/main/java/frc/robot/subsystems/ClimberSubsystem.java
+++ src/main/java/frc/robot/subsystems/ClimberSubsystem.java
@@ -1,11 +1,16 @@
 package frc.robot.subsystems;
 
+import static edu.wpi.first.units.Units.Rotations;
+
 import com.ctre.phoenix6.configs.MotorOutputConfigs;
 import com.ctre.phoenix6.controls.Follower;
 import com.ctre.phoenix6.hardware.TalonFX;
 import com.ctre.phoenix6.signals.NeutralModeValue;
 
+import edu.wpi.first.units.measure.Angle;
+import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.constants.ClimberConstants;
 import frc.robot.constants.Constants;
 
 public class ClimberSubsystem extends SubsystemBase{
@@ -14,6 +19,31 @@ public class ClimberSubsystem extends SubsystemBase{
 
     public ClimberSubsystem(){
         falconRight.getConfigurator().apply(new MotorOutputConfigs().withNeutralMode(NeutralModeValue.Brake));
+        falconRight.setPosition(Rotations.of(0));
         falconLeft.setControl(new Follower(Constants.CLIMBER_FALCON_RIGHT_ID, true));
+    }
+
+    public Angle getPosition(){
+        return falconRight.getPosition().getValue();
+    }
+
+    private void out(){
+        falconRight.setVoltage(ClimberConstants.OUT_VOLTS);
+    }
+
+    private void climb(){
+        falconRight.setVoltage(ClimberConstants.CLIMB_VOLTS);
+    }
+
+    private void stop(){
+        falconRight.setVoltage(0);
+    }
+
+    public Command outCommand(){
+        return startEnd(() -> out(), () -> stop()).until(() -> getPosition().equals(ClimberConstants.OUTER_LIMIT));
+    }
+
+    public Command climbCommand(){
+        return startEnd(() -> climb(), () -> stop()).until(() -> getPosition().equals(ClimberConstants.INNER_LIMIT));
     }
 }
