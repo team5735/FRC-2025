150a19aab9bda81f3ea034577c78a5a28049a383
Framework laptop controlfreaks account <please.use.your.own@account.com>
2025-02-24 20:22:46 -0500

    why...

--- src/main/java/frc/robot/constants/AlgaeConstants.java
+++ src/main/java/frc/robot/constants/AlgaeConstants.java
@@ -1,6 +1,6 @@
 package frc.robot.constants;
 
 public class AlgaeConstants {
-    public static final double GRAB_VOLTS = 10.0;
-    public static final double SPIT_VOLTS = 10.0;
+    public static final double GRAB_VOLTS = 5.0;
+    public static final double SPIT_VOLTS = 5.0;
 }
