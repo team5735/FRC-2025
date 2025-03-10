363f27f52aae723b18a586ade992cd631682c40b
Sebastian Gracia <sebastian.r.gracia@gmail.com>
2025-03-10 17:08:18 -0400

    draft

---
+++ src/main/java/frc/robot/commands/vision/PIDToNearestBranch.java
@@ -0,0 +1,22 @@
+package frc.robot.commands.vision;
+
+import java.util.function.Supplier;
+
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.DrivetrainSubsystem;
+import frc.robot.subsystems.VisionSubsystem;
+
+public class PIDToNearestBranch extends Command {
+    private DrivetrainSubsystem drivetrain;
+    private VisionSubsystem vision;
+    private Supplier<Translation2d> targetSupplier;
+
+    public PIDToNearestBranch(DrivetrainSubsystem drivetrain, VisionSubsystem vision,
+            Supplier<Translation2d> targetSupplier) {
+        this.drivetrain = drivetrain;
+        this.vision = vision;
+        this.targetSupplier = targetSupplier;
+        addRequirements(drivetrain, vision);
+    }
+}
