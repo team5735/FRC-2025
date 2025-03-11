410a88bed23f2a105f3131c8fa2ff811595b59cd
Arya <aryadsam30@gmail.com>
2025-03-11 18:50:12 -0400

    Change flipper voltage

--- src/main/java/frc/robot/constants/CoralConstants.java
+++ src/main/java/frc/robot/constants/CoralConstants.java
@@ -5,6 +5,8 @@ import static edu.wpi.first.units.Units.Seconds;
 import edu.wpi.first.units.measure.Time;
 
 public class CoralConstants {
+    // TODO FIND ACTUAL VOLTAGES FOR INTAKE
+    // TODO FIND ACTUAL DELAY
     public static final double INTAKE_TOP_VOLTS = .75;
     public static final double INTAKE_BOTTOM_VOLTS = .75;
 
@@ -16,11 +18,11 @@ public class CoralConstants {
     public static final double BRANCH_BOTTOM_VOLTS = 1.0;
     public static final Time BRANCH_TIMEOUT = Seconds.of(2);
 
-    public static final double FLIPPER_VOLTS = 2.0;
-    public static final double UNFLIP_VOLTS = 1.5;
+    public static final double FLIPPER_VOLTS = 1.0;
+    public static final double UNFLIP_VOLTS = 1.0;
 
-    public static final double FEEDER_VOLTS = 0.5;
-    public static final double UNFEED_VOLTS = -1.0;
+    public static final double FEEDER_VOLTS = 2.5;
+    public static final double UNFEED_VOLTS = -3.0;
 
     public static final Time FEED_DELAY_SECONDS = Seconds.of(1.5);
     public static final Time L4_EJECTION_TIMEOUT = Seconds.of(1.5);
