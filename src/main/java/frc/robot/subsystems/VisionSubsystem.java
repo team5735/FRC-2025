dd59c10b57f27beea5f73013c178c1bbc22d5af1
Sebastian Gracia <sebastian.r.gracia@gmail.com>
2025-03-22 17:19:03 -0400

    Revert "don't fetch stddevs"
    This reverts commit 55725555192173590a0fbe45041a3f43c046a8ad.

--- src/main/java/frc/robot/subsystems/VisionSubsystem.java
+++ src/main/java/frc/robot/subsystems/VisionSubsystem.java
@@ -70,7 +70,11 @@ public class VisionSubsystem extends SubsystemBase {
             SmartDashboard.putNumber("poseestimator_status", 0);
         }
 
-        drivetrain.setVisionMeasurementStdDevs(VecBuilder.fill(0.7, 0.7, 9999999));
+        double[] stddevs = NetworkTableInstance.getDefault().getTable(limelight_name).getEntry("stddevs")
+                .getDoubleArray(new double[12]);
+        double mt2xdev = stddevs[6];
+        double mt2ydev = stddevs[7];
+        drivetrain.setVisionMeasurementStdDevs(VecBuilder.fill(mt2xdev, mt2ydev, 9999999));
         drivetrain.addVisionMeasurement(
                 mt2.pose,
                 mt2.timestampSeconds);
