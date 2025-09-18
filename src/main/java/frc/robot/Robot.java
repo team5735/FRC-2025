0df99b68f1e7a3055062090364d4c49a81e29f53
Sebastian Gracia <sebastian.r.gracia@gmail.com>
2025-09-17 20:22:22 -0400

    put the commandscheduler in smartdashboard

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -5,6 +5,7 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 
@@ -15,6 +16,7 @@ public class Robot extends TimedRobot {
 
     public Robot() {
         m_robotContainer = new RobotContainer();
+        SmartDashboard.putData(CommandScheduler.getInstance());
     }
 
     @Override
@@ -56,6 +58,7 @@ public class Robot extends TimedRobot {
         if (m_autonomousCommand != null) {
             m_autonomousCommand.cancel();
         }
+        RobotContainer.vision.scheduleWaitForApriltagCommand();
         // RobotContainer.coraler.flipperTimedReset().schedule();
         // RobotContainer.elevator.toLevelCommand(Level.BASE).schedule();
     }
