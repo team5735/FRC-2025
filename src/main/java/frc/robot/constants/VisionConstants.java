4eb401c4137accad3aa87e98ea0a2f85cd3a0d6c
Sebastian Gracia <sebastian.r.gracia@gmail.com>
2026-01-17 13:30:50 -0500

    disable mt2 again due to testing results

--- src/main/java/frc/robot/constants/VisionConstants.java
+++ src/main/java/frc/robot/constants/VisionConstants.java
@@ -16,7 +16,7 @@ public class VisionConstants {
 
     public static final Distance PID_DRIVE_THRESHOLD = Meters.of(0.5);
 
-    public static final boolean IS_MT2 = true;
+    public static final boolean IS_MT2 = false;
 
     public static final Distance RESET_PIGEON_DISTANCE = Meters.of(105);
     public static final Time RESET_PIGEON_INTERVAL = Seconds.of(1);
