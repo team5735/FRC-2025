9f4da978c63bab37b3727ecc1962333478e62eba
ZWORX52 <66217856+ZWORX52@users.noreply.github.com>
2026-01-23 18:27:47 -0500

    fix package and imports of the unit test

--- src/test/java/frc/pathplanner/PathPlannerTest.java
+++ src/test/java/frc/pathplanner/PathPlannerTest.java
@@ -1,17 +1,14 @@
-import org.junit.jupiter.api.Test;
-import java.util.List;
+package frc.pathplanner;
 
-import com.pathplanner.lib.path.GoalEndState;
-import com.pathplanner.lib.path.IdealStartingState;
+import org.junit.jupiter.api.Test;
+
 import com.pathplanner.lib.path.PathConstraints;
 import com.pathplanner.lib.path.PathPlannerPath;
-import com.pathplanner.lib.path.Waypoint;
 import com.pathplanner.lib.trajectory.PathPlannerTrajectory;
 
+import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
-import edu.wpi.first.math.geometry.Pose2d;
-import edu.wpi.first.math.geometry.Rotation2d;
 import frc.robot.constants.drivetrain.CompbotConstants;
 import frc.robot.util.PathPlannerHelpers;
 
@@ -23,22 +20,22 @@ public class PathPlannerTest {
         PathConstraints constraints = bot.getPathFollowConstraints();
 
         PathPlannerPath path = PathPlannerHelpers.createLinearPath(
-            new Translation2d(0,0), 
-            new Translation2d(1,1), 
-            Rotation2d.fromDegrees(-45), 
-            new PathConstraints(3.0, 2.0, 360, 540));
-            
-        PathPlannerTrajectory trajectory = path.generateTrajectory(new ChassisSpeeds(), Rotation2d.fromDegrees(0),bot.getConfig());
-        
+                new Translation2d(0, 0),
+                new Translation2d(1, 1),
+                Rotation2d.fromDegrees(-45),
+                new PathConstraints(3.0, 2.0, 360, 540));
+
+        PathPlannerTrajectory trajectory = path.generateTrajectory(new ChassisSpeeds(), Rotation2d.fromDegrees(0),
+                bot.getConfig());
+
         // Sample points for plotting
         System.out.printf("x_meters, y_meters, rotation_degrees\n");
         for (var s : trajectory.getStates()) {
             Translation2d pos = s.pose.getTranslation();
             System.out.printf("%.2f, %.2f, %.2f\n",
-                pos.getX(),
-                pos.getY(),
-                s.pose.getRotation().getDegrees()
-            );
+                    pos.getX(),
+                    pos.getY(),
+                    s.pose.getRotation().getDegrees());
         }
     }
 }
